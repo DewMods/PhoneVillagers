1e5b6d7ef2eaeb04437be3c28d7e268e4086fc71
DewMods <DewMods@gmail.com>
2018-08-17 20:53:17 +1000

    - Removed debugging hotkeys

--- Phone Villagers/ModEntry.cs
+++ Phone Villagers/ModEntry.cs
@@ -70,59 +70,6 @@ namespace DewMods.StardewValleyMods.PhoneVillagers
 
             switch (e.Button)
             {
-                case SButton.F9:
-                    foreach (var l in Game1.locations)
-                    {
-                        foreach (var npc in l.characters)
-                        {
-                            this.Monitor.Log($"NPC: {npc.getName()}. Location: {npc.currentLocation.Name}. Can Socialise: {npc.CanSocialize}. Can Talk: {npc.canTalk()}. Is Invisible {npc.IsInvisible}");
-                        }
-                    }
-                    break;
-                case SButton.F10:
-                    if (Game1.activeClickableMenu is GameMenu menu)
-                    {
-                        var pages = this.Helper.Reflection.GetField<List<IClickableMenu>>(menu, "pages").GetValue();
-                        if (pages[menu.currentTab] is StardewValley.Menus.SocialPage socialPage)
-                        {
-                            pages[menu.currentTab] = new Menus.SocialPage(this, socialPage.xPositionOnScreen, socialPage.yPositionOnScreen, socialPage.width, socialPage.height);
-                        }
-                    }
-
-                    break;
-                case SButton.F11:
-                    var firstNpc = Game1.locations.FirstOrDefault(
-                        l => l.characters.Count(c => c.isVillager() && c.canTalk()) > 0)?.characters.FirstOrDefault(
-                        c => c.isVillager() && c.canTalk());
-
-                    var who = Game1.player;
-
-                    if (firstNpc != null)
-                    {
-                        // Get NPC dialogue
-                        bool flag1 = false;
-                        if (who.friendshipData.ContainsKey(firstNpc.Name))
-                        {
-                            flag1 = firstNpc.checkForNewCurrentDialogue(who.friendshipData[firstNpc.Name].Points,
-                                false);
-                            if (!flag1)
-                                flag1 = firstNpc.checkForNewCurrentDialogue(who.friendshipData[firstNpc.Name].Points,
-                                    true);
-                        }
-
-                        if (firstNpc.CurrentDialogue.Count == 0)
-                        {
-                            // NPC has no dialogue, no answer
-                            Game1.drawDialogueNoTyping("*Dial tone*...");
-                        }
-                        else
-                        {
-                            // Display NPC dialogue
-                            Game1.drawDialogue(firstNpc);
-                        }
-                    }
-
-                    break;
                 default:
                     break;
             }
