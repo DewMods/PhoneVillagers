b309f46c7809effdbdddd9c48043f2acbfd7f952
DewMods <DewMods@gmail.com>
2018-08-19 14:51:33 +1000

    #7 Refactoring
    - Added Common lib
    - Added debug output to check fishing experience is gained correctly
    - Added nexus mod version checking

---
+++ Common/Enums.cs
@@ -0,0 +1,16 @@
+﻿namespace DewMods.StardewValleyMods.Common.Enums
+{
+    public static class Player
+    {
+        // experiencePoints index
+        public enum ExperiencePointsIndex
+        {
+            Farming = 0,
+            Fishing = 1,
+            Foraging = 2,
+            Mining = 3,
+            Combat = 4,
+            Luck = 5,
+        }
+    }
+}
--- NoFishingMinigame/ModEntry.cs
+++ NoFishingMinigame/ModEntry.cs
@@ -8,26 +8,22 @@ using StardewModdingAPI.Events;
 using StardewValley;
 using StardewValley.Menus;
 using StardewValley.Tools;
+using DewMods.StardewValleyMods.Common.Enums;
 
-namespace NoFishingMinigame
+namespace DewMods.StardewValleyMods.SkipFishingMinigame
 {
     public class ModEntry : Mod
     {
         public override void Entry(IModHelper helper)
         {
             MenuEvents.MenuChanged += MenuEvents_MenuChanged;
-
-#if DEBUG
-            // TESTING
             InputEvents.ButtonPressed += InputEvents_ButtonPressed;
-#endif
         }
 
         private void MenuEvents_MenuChanged(object sender, EventArgsClickableMenuChanged e)
         {
             Log($"MenuEvents_MenuChanged {e.NewMenu}");
             Log($"Game1.player.CurrentTool {Game1.player.CurrentTool}");
-            Log($"Game1.dailyLuck {Game1.dailyLuck}");
 
             if (e.NewMenu is BobberBar bobberBar && Game1.player.CurrentTool is FishingRod fishingRod)
             {
@@ -38,17 +34,13 @@ namespace NoFishingMinigame
                 var perfect = this.Helper.Reflection.GetField<bool>(bobberBar, "perfect").GetValue();
                 var treasure = this.Helper.Reflection.GetField<bool>(bobberBar, "treasure").GetValue();
 
-                if (Game1.player.fishCaught.TryGetValue(whichFish, out var whichFishCaughtTimes))
-                {
-                    Log($"whichFishCaughtTimes   [{string.Join(", ", whichFishCaughtTimes)}]");
-                }
-
-                Log($"whichFish   [{whichFish}]. Times caught [{whichFishCaughtTimes?[0] + 1}]. Largest [{whichFishCaughtTimes?[1]}].");
-                Log($"fishSize    [{fishSize}]");
-                Log($"fishQuality [{fishQuality}]");
-                Log($"difficulty  [{difficulty}]");
-                Log($"perfect     [{perfect}]");
-                Log($"treasure    [{treasure}]");
+                Game1.player.fishCaught.TryGetValue(whichFish, out var whichFishCaughtTimes);
+                Log($"whichFish       [{whichFish}]. Times caught [{whichFishCaughtTimes?[0] + 1}]. Largest [{whichFishCaughtTimes?[1]}].");
+                Log($"fishSize        [{fishSize}]");
+                Log($"fishQuality     [{fishQuality}]");
+                Log($"difficulty      [{difficulty}]");
+                Log($"perfect         [{perfect}]");
+                Log($"treasure        [{treasure}]");
 
                 // We always catch the treasure if one was spawned
                 var treasureCaught = treasure;
@@ -70,11 +62,43 @@ namespace NoFishingMinigame
             if (!Context.IsWorldReady)
                 return;
 
-            if (e.Button == SButton.MouseLeft && Game1.player.CurrentTool is FishingRod fishingRod)
+            if (e.Button == SButton.MouseLeft && Game1.player.CurrentTool is FishingRod fishingRod && Game1.player.CanMove)
             {
+#if DEBUG
+                // Output Fishing EXP
+                var fishingExp = Game1.player.experiencePoints[(int)Player.ExperiencePointsIndex.Fishing];
+                Log($"Fishing Experience [{fishingExp}]");
+
+                // TESTING Higher chance of treasure
                 Game1.player.LuckLevel = 100;
                 Log($"OVERRIDING Game1.player.LuckLevel: [{Game1.player.LuckLevel}]", LogLevel.Alert);
+#endif
             }
+
+            //var oldFarmingExp = Game1.player.experiencePoints[(int)Player.ExperiencePointsIndex.Farming];
+            //var oldFishingExp = Game1.player.experiencePoints[(int)Player.ExperiencePointsIndex.Fishing];
+            //var oldMiningExp = Game1.player.experiencePoints[(int)Player.ExperiencePointsIndex.Mining];
+            //var oldCombatExp = Game1.player.experiencePoints[(int)Player.ExperiencePointsIndex.Combat];
+            //var oldUnknownExp = Game1.player.experiencePoints[(int)Player.ExperiencePointsIndex.UNKNOWN];
+            //var oldForagingExp = Game1.player.experiencePoints[(int)Player.ExperiencePointsIndex.Foraging];
+
+            //var newFarmingExp = Game1.player.experiencePoints[(int)Player.ExperiencePointsIndex.Farming];
+            //Log($"Farming Experience [{oldFarmingExp}] -> [{newFarmingExp}] (+{newFarmingExp - oldFarmingExp})");
+
+            //var newFishingExp = Game1.player.experiencePoints[(int)Player.ExperiencePointsIndex.Fishing];
+            //Log($"Fishing Experience [{oldFishingExp}] -> [{newFishingExp}] (+{newFishingExp - oldFishingExp})");
+
+            //var newMiningExp = Game1.player.experiencePoints[(int)Player.ExperiencePointsIndex.Mining];
+            //Log($"Mining Experience [{oldMiningExp}] -> [{newMiningExp}] (+{newMiningExp - oldMiningExp})");
+
+            //var newCombatExp = Game1.player.experiencePoints[(int)Player.ExperiencePointsIndex.Combat];
+            //Log($"Combat Experience [{oldCombatExp}] -> [{newCombatExp}] (+{newCombatExp - oldCombatExp})");
+
+            //var newUnknownExp = Game1.player.experiencePoints[(int)Player.ExperiencePointsIndex.UNKNOWN];
+            //Log($"UNKNOWN Experience [{oldUnknownExp}] -> [{newUnknownExp}] (+{newUnknownExp - oldUnknownExp})");
+
+            //var newForagingExp = Game1.player.experiencePoints[(int)Player.ExperiencePointsIndex.Foraging];
+            //Log($"Foraging Experience [{oldForagingExp}] -> [{newForagingExp}] (+{newForagingExp - oldForagingExp})");
         }
 
         /// <summary>
